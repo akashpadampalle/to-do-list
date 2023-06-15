a9de4e24c2ecf1dd6f1dae3713b391fc95743011
Akash Padampalle <akashpadampalle780@gmail.com>
2023-06-15 21:04:26 +0530

    change mongodb url

--- config/connectiondb.js
+++ config/connectiondb.js
@@ -1,21 +1,18 @@
-
 // requiring mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //connecting to mongodb
-mongoose.connect('mongodb://localhost:27017/todolist_db');
+mongoose.connect("mongodb://127.0.0.1:27017/todolist_db");
 
 //getting connection to check whether it is successfull or not
 const db = mongoose.connection;
 
-
 //print error if there
-db.on('error', console.error.bind(console, "Error to connect"));
+db.on("error", console.error.bind(console, "Error to connect"));
 
 //print successfull message
-db.once('open', function(){
-    console.log('connect to database 👌🏻')
+db.once("open", function () {
+  console.log("connect to database 👌🏻");
 });
-
 
 module.exports = db;
